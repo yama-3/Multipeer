670f7aefd7583c5b43e9dc11403d9e7c5908a19a
Kazuhiro Yamabe <kazuhiro.yamabe@gmail.com>
2017-09-18 20:50:07 +0900

    Work correctly

--- Peer/FirstViewController.swift
+++ Peer/FirstViewController.swift
@@ -7,19 +7,61 @@
 //
 
 import UIKit
+import MultipeerConnectivity;
 
 class FirstViewController: UIViewController {
 
+    @IBOutlet weak var connectionsLabel: UILabel!
+
+    let colorService = ColorServiceManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        colorService.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func redTapped() {
+        self.change(color: .red)
+        colorService.send(colorName: "red")
+    }
+    
+    @IBAction func yellowTapped() {
+        self.change(color: .yellow)
+        colorService.send(colorName: "yellow")
+    }
 
-
+    func change(color : UIColor) {
+        UIView.animate(withDuration: 0.2) {
+            self.view.backgroundColor = color
+        }
+    }
 }
 
+extension FirstViewController : ColorServiceManagerDelegate {
+    
+    func connectedDevicesChanged(manager: ColorServiceManager, connectedDevices: [String]) {
+        OperationQueue.main.addOperation {
+            self.connectionsLabel.text = "Connections: \(connectedDevices)"
+        }
+    }
+    
+    func colorChanged(manager: ColorServiceManager, colorString: String) {
+        OperationQueue.main.addOperation {
+            switch colorString {
+            case "red":
+                self.change(color: .red)
+            case "yellow":
+                self.change(color: .yellow)
+            default:
+                NSLog("%@", "Unknown color value received: \(colorString)")
+            }
+        }
+    }
+    
+}
